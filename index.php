d8acc89d65688ef39b1e5da784ee50ecd39a31ef
Dillon Ellis <dellis@fcul.com>
2022-08-09 10:10:38 -0400

    remove old files and add original about-inx file

--- index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-<html>
-<head>
-<title>UMC</title>
-<meta http-equiv="REFRESH" content="0;url=https://www.savannahpostalcu.com/main.php"></HEAD>
-<BODY>
-
-</BODY>
-</HTML>
