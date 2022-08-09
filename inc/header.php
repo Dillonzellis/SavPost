d92b3c30899f3dc58377748976dce1ee0ec88fa2
Dillon Ellis <dellis@fcul.com>
2022-08-09 16:25:21 -0400

    original footer includes

--- inc/header.php
+++ inc/header.php
@@ -1,22 +1,28 @@
 <div class="container">
-                <div class="site-logo">
-                    <a href="https://www.savannahpostalcu.com/index.php"><img src="https://www.savannahpostalcu.com/images/logo.png" alt="Savannah Postal Credit Union Logo"></a>
+    <div class="site-logo">
+        <a href="https://www.savannahpostalcu.com/index.php"><img src="https://www.savannahpostalcu.com/images/logo.png" alt="Savannah Postal Credit Union Logo"></a>
+    </div>
+    <a href="#" class="btn btn-default btn-sm" id="contact-info" aria-label="Mobile Navigation"><i class="fa fa-bars"></i></a>
+    <div class="site-header-right">
+        <ul class="header-info-cols">
+            <li>
+                <span class="icon-col"><i class="fa fa-map-marker"></i></span>
+                <div>
+                    <div><span>22 Oglethorpe Professional Blvd.<br>Savannah, GA 31406</span></div>
                 </div>
-                <a href="#" class="btn btn-default btn-sm" id="contact-info" aria-label="Mobile Navigation"><i class="fa fa-bars"></i></a>
-                <div class="site-header-right">
-                	<ul class="header-info-cols">
-  						<li>
-                        	<span class="icon-col"><i class="fa fa-map-marker"></i></span>
-                            <div><div><span>22 Oglethorpe Professional Blvd.<br>Savannah, GA 31406</span></div></div>
-                        </li>
-  						<li>
-                        	<span class="icon-col"><i class="fa fa-phone"></i></span>
-                            <div><div><span><strong>Call us </strong><br>(912) 691-2087</span></div></div>
-                        </li>
-  						<li>
-                        	<span class="icon-col"><i class="fa fa-envelope-o"></i></span>
-							<div><div><span><strong>Email Us</strong><br><a href="mailto:spcu@spcu22.com" style="color: black;">spcu@spcu22.com</a></span></div></div>
-                        </li>
-                    </ul>
+            </li>
+            <li>
+                <span class="icon-col"><i class="fa fa-phone"></i></span>
+                <div>
+                    <div><span><strong>Call us </strong><br>(912) 691-2087</span></div>
                 </div>
-            </div>
+            </li>
+            <li>
+                <span class="icon-col"><i class="fa fa-envelope-o"></i></span>
+                <div>
+                    <div><span><strong>Email Us</strong><br><a href="mailto:spcu@spcu22.com" style="color: black;">spcu@spcu22.com</a></span></div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
