ae007490d42d68c8d3bbe4d04bdd32b313be3cf3
Dillon Ellis <dellis@fcul.com>
2022-08-09 11:11:57 -0400

    original nav menu

---
+++ inc/nav.php
@@ -0,0 +1,156 @@
+<div class="container">
+    <a href="#" id="menu-toggle">Menu</a>
+    <nav>
+        <ul class="dd-menu sf-menu">
+            <li><a href="https://www.savannahpostalcu.com/index.php">Home</a></li>
+            <li class="megamenu"><a href="https://www.savannahpostalcu.com/pages/savings-index.php" title="Savings">Savings</a>
+                <ul class="dropdown">
+                    <li>
+                        <div class="megamenu-container container">
+                            <div class="row">
+                                <div class="col-md-3 megamenu-col">
+                                    <ul class="sub-menu">
+                                        <li><a href="https://www.savannahpostalcu.com/pages/savings-accounts.php">Savings Accounts</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/club-accounts.php">Club Accounts</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/savings-cert.php">Savings Certificates</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/iras.php">Individual Retirement Accounts</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-3 megamenu-col">
+                                    <ul class="sub-menu">
+
+                                        <li><a href="https://www.savannahpostalcu.com/pages/direct-deposit.php">Direct Deposit</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/payroll-deduction.php">Payroll Deduction</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/federal-insurance.php">Federal Insurance (NCUA)</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-6 megamenu-col">
+                                    <img src="https://www.savannahpostalcu.com/images/savings-inx.jpg" alt="">
+                                </div>
+
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </li>
+            <li class="megamenu"><a href="https://www.savannahpostalcu.com/pages/checking-index.php" title="Checking">Checking</a>
+                <ul class="dropdown">
+                    <li>
+                        <div class="megamenu-container container">
+                            <div class="row">
+                                <div class="col-md-3 megamenu-col">
+                                    <ul class="sub-menu">
+                                        <li><a href="https://www.savannahpostalcu.com/pages/checking-accounts.php">Checking Accounts</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/debit-cards.php">Debit Cards</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/fee-schedule.php">Fee Schedule</a></li>
+                                        <!-- <li><a href="#" data-toggle="modal" data-target="#check-ordering">Check Ordering</a></li> -->
+                                    </ul>
+                                </div>
+                                <div class="col-md-3 megamenu-col">
+                                    <ul class="sub-menu">
+                                        <li><a href="https://www.savannahpostalcu.com/pages/direct-deposit.php">Direct Deposit</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/payroll-deduction.php">Payroll Deduction</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/overdraft-protection.php">Overdraft Protection</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/contact-us.php">Report Lost or Stolen Card</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-6 megamenu-col">
+                                    <img src="https://www.savannahpostalcu.com/images/checking-inx.jpg" alt="Checking" title="Checking">
+                                </div>
+
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </li>
+            <li class="megamenu"><a href="https://www.savannahpostalcu.com/pages/loans-index.php" title="Loans">Loans</a>
+                <ul class="dropdown">
+                    <li>
+                        <div class="megamenu-container container">
+                            <div class="row">
+                                <div class="col-md-3 megamenu-col">
+                                    <ul class="sub-menu">
+                                        <li><a href="https://www.savannahpostalcu.com/pages/auto-loans.php" title="Auto Loans">Auto Loans</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/visa-credit-cards.php" title="Credit Cards">Credit Cards</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/heloc.php" title="Home Equity Loans">Home Equity Loans</a></li>
+                                        <li><a href="#" title="Mortgage Loans" data-toggle="modal" data-target="#mortgage">Mortgage Loans</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/personal-loans.php" title="Personal Loans">Personal Loans</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/recreational-vehicle-loans.php" title="Recreational Vehicle Loans">Recreational Vehicle Loans</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-3 megamenu-col">
+                                    <ul class="sub-menu">
+                                        <li><a href="https://www.savannahpostalcu.com/pages/share-secured-loans.php" title="Shared Secured Loans">Shared Secured Loans</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/payment-protection.php" title="Payment Protection">Payment Protection</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pdf/loan.pdf" title="Loan Application" target="_blank">Loan Application</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pdf/credit-card-app.pdf" title="Credit Card Application" target="_blank">Credit Card Application</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/financial-calculators.php" title="Financial Calculators">Financial Calculators</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-6 megamenu-col">
+                                    <img src="https://www.savannahpostalcu.com/images/loans-inx.jpg" alt="Checking" title="Checking">
+                                </div>
+
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </li>
+            <li class="megamenu"><a href="https://www.savannahpostalcu.com/pages/services-index.php" title="Loans">Services</a>
+                <ul class="dropdown">
+                    <li>
+                        <div class="megamenu-container container">
+                            <div class="row">
+                                <div class="col-md-3 megamenu-col">
+                                    <ul class="sub-menu">
+                                        <li><a href="https://www.savannahpostalcu.com/pages/newsletters.php" title="Newsletter">Newsletter</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/sixflags.php" title="Six Flags">Six Flags Discounted Tickets</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/phone.php" title="SUZI/24 Hour Audio Response">SUZI/24 Hour Audio Response</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/idtheft-info.php" title="Identity Theft Information">Identity Theft Information</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/gap.php" title="Guaranteed Auto Protection">Guaranteed Auto Protection</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/add.php" title="AD&D Insurance">AD&D Insurance</a></li>
+                                        <li><a href="#" data-toggle="modal" data-target="#check-ordering">Check Ordering</a></li>
+                                        <li><a href="#" data-toggle="modal" data-target="#ezcardinfo">Credit Card Account Info</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-3 megamenu-col">
+                                    <ul class="sub-menu">
+                                        <li><a href="https://www.savannahpostalcu.com/pages/trustage.php" title="TruStage™ Auto & Homeowners Insurance Program">TruStage™ Auto & Homeowners Insurance Program</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/members-financial-network.php" title="MEMBERS Financial Network">MEMBERS Financial Network</a></li>
+                                        <li><a href="#" data-toggle="modal" data-target="#cuna-brokerage">CUNA Brokerage</a></li>
+                                        <li><a href="#" data-toggle="modal" data-target="#cuna-investments">Investment Products</a></li>
+                                        <li><a href="#" data-toggle="modal" data-target="#cuna-insurance">Insurance Products</a></li>
+                                        <li><a href="https://www.savannahpostalcu.com/pages/financial-calculators.php" title="Financial Calculators">Financial Calculators</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-6 megamenu-col">
+                                    <img src="https://www.savannahpostalcu.com/images/services-inx.jpg" alt="Services" title="Services">
+                                </div>
+
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </li>
+            <li><a href="https://www.savannahpostalcu.com/pages/about-inx.php">About Us</a>
+                <ul class="sub-menu">
+                    <li><a href="https://www.savannahpostalcu.com/pages/who-we-are.php" title="Who We Are">Who We Are</a></li>
+                    <li><a href="https://www.savannahpostalcu.com/pages/who-can-join.php" title="Who Can Join?">Who Can Join?</a></li>
+                    <li><a href="https://www.savannahpostalcu.com/pages/board-directors.php" title="Board of Directors">Board of Directors</a></li>
+                    <li><a href="https://www.savannahpostalcu.com/pages/staff-directory.php" title="Staff Directory">Staff Directory</a></li>
+                    <li><a href="https://www.savannahpostalcu.com/pages/privacy-policy.php" title="Privacy Policy">Privacy Policy</a></li>
+                    <li><a href="https://www.savannahpostalcu.com/pages/contact-us.php" title="Contact Us">Contact Us</a></li>
+                </ul>
+            </li>
+            <li><a href="https://www.savannahpostalcu.com/pages/forms-index.php" title="Application">Applications</a>
+                <ul class="sub-menu">
+                    <li><a href="https://www.savannahpostalcu.com/pdf/loan.pdf" target="_blank">Loan Application</a></li>
+                    <li><a href="https://www.savannahpostalcu.com/pdf/credit-card-app.pdf" target="_blank">Credit Card Application</a></li>
+                    <li><a href="https://www.savannahpostalcu.com/pdf/debit-card-app.pdf" target="_blank">Debit Card Application</a></li>
+                    <li><a href="https://www.savannahpostalcu.com/pdf/cardholder-dispute-form.pdf" target="_blank">Card Holder Dispute Form</a></li>
+                </ul>
+
+            </li>
+        </ul>
+    </nav>
+</div>
